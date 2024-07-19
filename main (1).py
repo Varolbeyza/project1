32a949d28cc5fe64b7aa950e0be71819639d97fb
Varolbeyza <147330122+Varolbeyza@users.noreply.github.com>
2024-07-19 21:16:50 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,33 @@
+# a=55; print("uzunluk:", len(str(a)))
+
+#a="erdinc\donmez"
+#print(a[:])
+
+
+#adi="Beyza"
+#soyAdi="Varol"
+#print(adi,end="/n")
+#print("Adı:", adi, "Soyadı:", soyAdi)
+#print(f"Adı: {adi}, Soyadı: {soyAdi}")
+
+
+#a="BEYZA VAROL"
+#print(a[:-1])
+#print(a[:1])
+#print(a[1:5])
+#print(a[1:8:2])
+#print(len(a))
+#print(a[1:10:3])
+#print(a[1::])
+#print(a[::2])
+#print(a[:6:2])
+#print(a[::-1])
+
+#adi = input("adınızı giriniz:")
+#soyAdi = input("soyadınızı giriniz:")
+#yas = input("yasınızı giriniz:")
+# print(f"Adı: {adi}, Soyadı: {soyAdi}, Yas: {yas}" )
+
+sayi1 = int(input("1. sayıyı giriniz:"))
+sayi2 = int(input("2. sayıyı giriniz:"))
+print(f"Toplam: {sayi1+sayi2}")
